e1adaad824028d1ddd6453a5f6af2f3ef080803f
Kostiantyn Kostikov <kostiantyn.kostikov@onereach.com>
2021-12-30 15:54:31 +0200

    ci: 🎡 ci settings

--- eslint-config/.eslintrc-vue.js
+++ eslint-config/.eslintrc-vue.js
@@ -16,9 +16,9 @@ module.exports = {
         indent: 'off',
         'vue/max-attributes-per-line': [2, { singleline: 1, multiline: 1 }],
         'vue/html-closing-bracket-newline': [2, { singleline: 'never', multiline: 'always' }],
-        'vue/attribute-hyphenation': 0,
-        'vue/require-prop-types': 0,
+        'vue/attribute-hyphenation': [2, 'always'],
         'vue/require-default-prop': 2,
+        'vue/prop-name-casing': [2, 'camelCase'],
         'vue/script-indent': ['error', 2, { 'baseIndent': 1 }],
         'vue/no-v-html': 2
       }
